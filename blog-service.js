c357c126fd45b5e77f6481739f40ce8cb2a10f58
ApplePieDough <130590477+ApplePieDough@users.noreply.github.com>
2023-06-02 17:31:50 -0400

    added functions to blog-service.js

--- blog-service.js
+++ blog-service.js
@@ -1,3 +1,66 @@
-function getPublishedPosts() {
-    console.log("/Blog route working")
+const fs = require('fs');
+
+let posts = [];
+let categories = [];
+
+function initialize() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/posts.json', 'utf8', (err, postData) => {
+      if (err) {
+        reject('Unable to read posts.json');
+        return;
+      }
+
+      posts = JSON.parse(postData);
+
+      fs.readFile('./data/categories.json', 'utf8', (err, categoryData) => {
+        if (err) {
+          reject('Unable to read categories.json');
+          return;
+        }
+
+        categories = JSON.parse(categoryData);
+
+        resolve();
+      });
+    });
+  });
 }
+
+function getAllPosts() {
+  return new Promise((resolve, reject) => {
+    if (posts.length > 0) {
+      resolve(posts);
+    } else {
+      reject('No posts available');
+    }
+  });
+}
+
+function getPublishedPosts() {
+  return new Promise((resolve, reject) => {
+    const publishedPosts = posts.filter(post => post.published);
+    if (publishedPosts.length > 0) {
+      resolve(publishedPosts);
+    } else {
+      reject('No published posts available');
+    }
+  });
+}
+
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length > 0) {
+      resolve(categories);
+    } else {
+      reject('No categories available');
+    }
+  });
+}
+
+module.exports = {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories
+};
