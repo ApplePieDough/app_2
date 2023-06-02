884b0454de8ac5449aff324f1ec9b783b4a60423
ApplePieDough <130590477+ApplePieDough@users.noreply.github.com>
2023-06-02 15:38:00 -0400

    edited server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Route '/' redirects to '/about'
+app.get("/", (req, res) => {
+    res.redirect("/about");
+});
+
+// Route '/about' returns the about.html file from the 'views' folder
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.listen(HTTP_PORT, () => {
+    console.log("Express http server listening on port " + HTTP_PORT);
+});
